a38da62a46104c2e8812235236d3a6b6260cd1aa
Dasha Demidova <striki06@mail.ru>
2023-11-06 16:18:31 +0300

    done

--- tasks/task_0.py
+++ tasks/task_0.py
@@ -1,5 +1,6 @@
 colors = ["Red", "Green", "White", "Black", "Pink", "Yellow"]
 
-...  # Ваш код
+colors[0] = "Yellow"
+colors[-1] = "Red"
 
 print(colors)
--- tasks/task_7.py
+++ tasks/task_7.py
@@ -1,5 +1,5 @@
 student_info = {"name": "Анна", "age": 20, "group_number": "А101"}
 
-keys_set = ...  # Ваш код
+keys_set = set(student_info)
 
 print(keys_set)
